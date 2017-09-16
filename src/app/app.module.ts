e9d8b82e3a75154b69bbaae9815f7ae501cf34f9
Guga Gongadze <ggong13@freeuni.edu.ge>
2017-09-16 16:40:50 +0400

    Added search, about, navbar components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: SearchComponent },
+      { path: 'about', component: AboutComponent },
+      { path: '**', redirectTo: '', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
